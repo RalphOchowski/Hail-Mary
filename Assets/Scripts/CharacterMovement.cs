15ed27dc2b6f72c8ebda2a1317eff0eacaae7436
Vedant Singh <ralphochowski@gmail.com>
2024-03-15 17:19:47 +0530

    03.15.2024, 17:20 - An attempt at pushing changes that were left behind during last push. Which consist of the reverted sprite list conversions and consequent broken animations.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,44 +8,45 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] Transform PlayerOrientation;
     [SerializeField] float speed = 8.0f;
     [SerializeField] float JumpPower = 5.0f;
-    Rigidbody2D Rigidbody;
+    Rigidbody2D rb;
     Animator CharacterAnimator;
-    float HorizontalMovement;
-    float VerticalMovement;
+    Vector2 HorizontalInput;
     bool IsFacingRight;
     void Start()
     {
-        Rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         CharacterAnimator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        HorizontalMovement = Input.GetAxisRaw("Horizontal");
-        Movement();
+        //HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        Walk();
     }
 
-    void Movement()
-    {
-        FlipSprite();
-        Rigidbody.velocity = new Vector2(HorizontalMovement * speed, Rigidbody.velocity.y);
-        if(HorizontalMovement != 0) CharacterAnimator.SetBool("IsWalking", true);
-        else CharacterAnimator.SetBool("IsWalking", false);
-        /*if (gameObject.GetComponent<CapsuleCollider2D>().IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
-        else if(Input.GetButtonDown("Jump")) Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, JumpPower);*/
-    }
 
     void OnJump(InputValue value)
     {
         if (value.isPressed)
         {
-            Rigidbody.velocity += new Vector2(0f, JumpPower);
+            rb.velocity += new Vector2(0f, JumpPower);
         }
+    }
+    void Walk()
+    {
+        Vector2 PlayerVelocity = new Vector2(HorizontalInput.x * speed, rb.velocity.y);
+        rb.velocity = PlayerVelocity;
+    }
+
+    void OnMove(InputValue value)
+    {
+
+        HorizontalInput = value.Get<Vector2>();
     }
 
     void FlipSprite()
     {
-        bool isPlayerMoving = Mathf.Abs(Rigidbody.velocity.x) > Mathf.Epsilon;
-        if(isPlayerMoving) PlayerOrientation.localScale = new Vector2(Mathf.Sign(Rigidbody.velocity.x), 1f);
+        bool isPlayerMoving = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if(isPlayerMoving) PlayerOrientation.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
     }
 }
